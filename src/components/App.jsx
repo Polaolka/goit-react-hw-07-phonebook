1ffd8ca9cd18ee40879b832eb7262f45dd1ab8b0
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-05 19:10:43 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,8 @@ import { PhoneBook } from './PhoneBook/PhoneBook';
 import { Filter } from './Filter/Filter';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchContacts } from "redux/operations";
-// import { selectError, selectIsLoading } from "redux/selectors";
 
 export default function App() {
-
-
   return (
     <>
       <Section title="Phonebook">
@@ -19,7 +13,7 @@ export default function App() {
       </Section>
       <Section title="Contacts">
         <Filter />
-                <ContactsList />
+        <ContactsList />
       </Section>
       <ToastContainer autoClose={2500} />
     </>
